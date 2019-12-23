c263e26aa52c189f2b61e8c20cab9a9a61635fa9
Rui <3116049c@icloud.com>
2019-12-24 02:12:53 +0900

    AnswerResultページのUI修正

--- src/components/AnswerResult.tsx
+++ src/components/AnswerResult.tsx
@@ -4,9 +4,16 @@ import _ from "lodash"
 import {useDispatch, useSelector} from "react-redux";
 import {Link} from "react-router-dom";
 import {Button} from "@material-ui/core";
+import ExpansionPanel from "@material-ui/core/ExpansionPanel";
+import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Typography from "@material-ui/core/Typography";
+import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+
 
 const AnswerResult = () => {
-    const dispatch = useDispatch()
     const result = useSelector((state: CombinedState) => state.result)
     const answer = useSelector((state: CombinedState) => state.answer)
     const question = useSelector((state: CombinedState) => state.question)
@@ -18,22 +25,64 @@ const AnswerResult = () => {
         }
     }
 
+    let comment = "";
+    if (collectNum === 10) {
+        comment = "パーフェクト！この調子で勉強しよう！"
+    } else if (collectNum <= 3) {
+        comment = "次はもっと頑張ろう。"
+    } else if (collectNum >= 4 && collectNum <= 7) {
+        comment = "毎日コツコツ勉強しよう。"
+    } else {
+        comment = "満点まであと少し！間違えた問題はしっかり復習しよう。"
+    }
+
     return (
-        <div>
-            <p>{collectNum} / 10問正解</p>
+        <Grid container xs={12}>
+            <Grid item container xs={12} justify={"center"}>
+                <Card style={{width: "90%", backgroundColor: "#005000", border: "solid 5px brown"}}>
+                    <Typography variant={"h5"} style={{color: "white", textAlign: "center", paddingTop: 30}}>{collectNum} / 10問正解</Typography>
+                    <Typography style={{color: "white", textAlign: "center", paddingTop: 30, paddingBottom: 40}}>{comment}</Typography>
+                </Card>
+            </Grid>
         {
             _.range(10).map((num) =>
-                <ul>
-                    <li>{result[num]}: 問題: {question[num].question} 正解: {question[num].answer} あなたの解答:{answer[num]}</li>
-                </ul>
+                <Grid item xs={12}>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary
+                            expandIcon={<ExpandMoreIcon />}
+                        >
+                            <Grid container xs={12}>
+                                <Grid item xs={3} justify={"center"}>
+                                    <Typography style={{color: result[num] === "O" ? "green" : "red"}}>{result[num]}</Typography>
+                                </Grid>
+                                <Grid item container xs={9} justify={"flex-end"}>
+                                    <Typography style={{color: result[num] === "O" ? "green" : "red"}}> 問題: {question[num].question}</Typography>
+                                </Grid>
+                            </Grid>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails style={{backgroundColor: "#e0e0e0"}} >
+                            <Grid container xs={12} direction={"column"}>
+                                <Grid item>
+                                    <Typography variant={"h6"}>正解: {question[num].answer}</Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Typography>あなたの解答: {answer[num]}</Typography>
+                                </Grid>
+                                <Grid item>
+                                    <Typography>解説: {question[num].description}</Typography>
+                                </Grid>
+                            </Grid>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </Grid>
             )
         }
-            <div>
+            <Grid item container xs={12} justify={"flex-end"}>
                 <Link to={"/home"}>
                     <Button variant={"contained"} color={"secondary"}>ホームへ戻る</Button>
                 </Link>
-            </div>
-        </div>
+            </Grid>
+        </Grid>
 
     )
 }
